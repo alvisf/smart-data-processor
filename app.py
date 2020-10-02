7e4ce77c67323bbd8a46e4ed7c0ecd3185e73f42
alvisf <alvisf0731@gmail.com>
2020-10-02 20:54:38 +0530

    dimension chenger with pillow

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, redirect, render_template,send_file
-from tasks import sentence_length
+from tasks import image_demension
+from PIL import Image
 
 app = Flask(__name__)
 @app.route('/')
@@ -9,9 +10,13 @@ def index():
 @app.route('/image',methods = ['POST'])
 def get_uploaded_image():
     img = request.files['image']
-    # print(img)
-    result = sentence_length.delay('sentence')
+    myImg = Image.open(img)  
+    myImg.show()
+    # result = image_demension.delay(img)
     return "uploaded successfully"
 
+    #TODO
+    # save img 
+    #send img location
 if __name__ == '__main__':
     app.run(debug=True)
--- tasks.py
+++ tasks.py
@@ -1,8 +1,26 @@
 from celery import Celery
+from PIL import Image  
 
 app = Celery('tasks', backend='rpc://', broker='pyamqp://')
 
 @app.task
-def sentence_length(sentence):
-    print("hello")
-    return len(sentence)
+def image_demension(img):
+    im = Image.open(img)  
+    width, height = im.size  
+    # Size of the image in pixels (size of orginal image)  
+    # (This is not mandatory)  
+    width, height = im.size  
+
+    # Setting the points for cropped image  
+    left = 4
+    top = height / 5
+    right = 154
+    bottom = 3 * height / 5
+
+    # Cropped image of above dimension  
+    # (It will not change orginal image)  
+    im1 = im.crop((left, top, right, bottom)) 
+    newsize = (300, 300) 
+    im1 = im1.resize(newsize) 
+    im1.show() 
+    return True
