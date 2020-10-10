e8d02e4253426209f7996aad6e4eb11abee44557
Sameena <sameenafathima99999@gmail.com>
2020-10-10 13:35:48 +0530

    Downloading image done

--- app.py
+++ app.py
@@ -2,26 +2,63 @@ from flask import Flask, jsonify, request, redirect, render_template,send_file,m
 from tasks import image_demension
 import os
 from PIL import Image
+from datetime import datetime
+
+
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'worker-img'
-@app.route('/')
-def index(): 
-    return render_template("index.html")
+app.config['UPLOAD_FOLDER'] = 'static/worker-img'
 
-@app.route('/image',methods = ['POST'])
-def get_uploaded_image():
-    img = request.files['image']
-    # myImg = Image.open(img)  
-    # myImg.show()
-    # result = image_demension.delay(img)
-    # save img 
-    img.save(os.path.join(app.config['UPLOAD_FOLDER'],img.filename))
-    #send img location
-    res = {
-        "Location":"worker-img/"+img.filename
+@app.route('/',methods = ['GET','POST'])
+def index(): 
+    if request.method == 'GET':
+        return render_template("index.html")
+    if request.method == 'POST':
+        img = request.files['image']
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        img.save(os.path.join(app.config['UPLOAD_FOLDER'],img.filename))
+        loc = "static/worker-img/"+img.filename
+        return render_template("download.html",loc = loc,start_time = current_time)
+
+@app.route('/send-task',methods = ['POST'])
+def send_task():
+    loc = request.json['loc']
+    myImg = Image.open(loc)   
+    result = image_demension(loc)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    result = image_demension(loc)
+    data = {
+        "time":current_time,
+        "img_ext":myImg.format.lower()
     }
-    return make_response(jsonify(res), 200)
+    return data
+
+
+
+# @app.route('/image',methods = ['GET','POST'])
+# def get_uploaded_image():
+#     if request.method == 'POST':
+#         img = request.files['image']
+#         print(img)
+#         now = datetime.now()
+#         current_time = now.strftime("%H:%M:%S")
+#         print("Current Time =", current_time)
+#         return render_template("download.html",time1 = current_time)
+  
+#      # save img 
+#     img.save(os.path.join(app.config['UPLOAD_FOLDER'],img.filename))
+#     loc = "static/worker-img/"+img.filename
+#     myImg = Image.open(loc)  
+#     # myImg.show()
+#     result = image_demension(loc)
+#     print("====================>",result)
+#      #send img location
+#     res = {
+#         "Location":"static/worker-img/"+img.filename
+#     }
+#     return render_template('download.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tasks.py
+++ tasks.py
@@ -1,15 +1,22 @@
 from celery import Celery
+from flask import Flask
 from PIL import Image  
+import os
+import time
+app1 = Flask(__name__)
+app = Celery('tasks', backend='rpc://', broker='amqp://')
+app1.config['UPLOAD_FOLDER'] = 'static/worker-img'
 
-app = Celery('tasks', backend='rpc://', broker='amqp://admin:mypass@rabbit:5672//:')
-
-@app.task
+@app.task()
 def image_demension(img):
+    #time.sleep(2)
     im = Image.open(img)  
     width, height = im.size  
-    # Size of the image in pixels (size of orginal image)  
-    # (This is not mandatory)  
-    width, height = im.size  
+    
+    # im.show()
+    # # Size of the image in pixels (size of orginal image)  
+    # # (This is not mandatory)  
+    # # width, height = im.size  
 
     # Setting the points for cropped image  
     left = 4
@@ -22,5 +29,7 @@ def image_demension(img):
     im1 = im.crop((left, top, right, bottom)) 
     newsize = (300, 300) 
     im1 = im1.resize(newsize) 
-    im1.show() 
+    width, height = im1.size  
+    print(width,height)
+    im1.save(os.path.join(app1.config['UPLOAD_FOLDER'],'cropped_img.'+im.format.lower()))
     return True
